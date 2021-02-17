f589e1c870aa62b75cb0526b8dcb0e9ce060b6b3
Oswaldo Puescas <opuescas@everis.com>
2021-02-16 21:25:27 -0500

    Initial commit

---
+++ src/app/shared/mocks/instructors.mocks.ts
@@ -0,0 +1,36 @@
+import { Instructor } from '../interfaces/instructor';
+
+export const InstructorsMock: Instructor[] = [
+
+    {
+        "id": 1,
+        "name": "Diego",
+        "lastName": "Quispe",
+        "educationTitle": "Expert CSS Advanced with SaaS",
+        "image": "http://examen-nh.jcramireztello.com/media/instructor/04.jpg"
+      
+    },
+    {
+        "id": 2,
+        "name": "Juan",
+        "lastName": "Estrada Lopez",
+        "educationTitle": "Expert Oracle Database Administration",
+        "image": "http://examen-nh.jcramireztello.com/media/instructor/01.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Emiliano",
+        "lastName": "Durand",
+        "educationTitle": "Expert Python Advanced",
+        "image": "http://examen-nh.jcramireztello.com/media/instructor/03.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Alberto",
+        "lastName": "Bonelli",
+        "educationTitle": " Expert Redes Cisco Specialist ",
+        "image": "http://examen-nh.jcramireztello.com/media/instructor/02.jpg"
+        
+    }
+
+]
